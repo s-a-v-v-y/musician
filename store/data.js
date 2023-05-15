3d8cb0e373496dd1f750a7662451823cf80e5bcf
Satvik Chandel <87795385+s-a-v-v-y@users.noreply.github.com>
2023-05-15 22:09:47 +0530

    Add files via upload

---
+++ store/data.js
@@ -0,0 +1,38 @@
+// A collection of musicians where the key is the unique id
+module.exports = {
+  coltrane: {
+    firstName: 'Arijit ',
+    lastName: 'Singh',
+    genre: 'Romantic',
+  },
+
+  davis: {
+    firstName: 'Jubin',
+    lastName: 'Nautiyal',
+    genre: 'Sad',
+  },
+
+  mccartney: {
+    firstName: 'Yo Yo Honey',
+    lastName: 'Singh',
+    genre: 'Rap',
+  },
+
+  hendrix: {
+    firstName: 'Armaan',
+    lastName: 'Malik',
+    genre: 'Romantic',
+  },
+
+  cobain: {
+    firstName: 'Ranjit',
+    lastName: 'Bawa',
+    genre: 'punjabi',
+  },
+
+  king: {
+    firstName: 'Shreya',
+    lastName: 'Ghoshal',
+    genre: 'Romantic',
+  },
+};
